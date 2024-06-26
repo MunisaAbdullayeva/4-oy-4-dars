7737cf75c2085c1100481926d24bb40295c35f44
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-06-26 14:37:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let ism = prompt("ism")
+
+// if (ism.length > 6) 
+// {
+//     console.log("zor")
+//  } else (ism.length < 6) 
+// {
+//     console.log("yomon")
+// }
+
+ if (ism.includes("b") || ism.includes("j") ) {
+     console.log(ism.toUpperCase())
+ } else {
+     console.log(ism.toLowerCase())
+ }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
